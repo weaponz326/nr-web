4c559ba300068efb69bbcd04de15ac7c40f93c35
Weps <weaponz326@gmail.com>
2022-08-17 15:26:08 +0000

    legalities pages

--- projects/personal/src/app/pages/legalities/legalities-routing.module.ts
+++ projects/personal/src/app/pages/legalities/legalities-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LegalitiesPage } from './legalities.page';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { TermsComponent } from './terms/terms.component';
+
+const routes: Routes = [
+  { 
+    path: "", 
+    component: LegalitiesPage,
+    children: [
+      { path: "", component: TermsComponent },
+      { path: "terms", component: TermsComponent },
+      { path: "privacy", component: PrivacyComponent },
+    ] 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- projects/personal/src/app/pages/legalities/legalities.module.ts
+++ projects/personal/src/app/pages/legalities/legalities.module.ts
@@ -2,16 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LegalitiesRoutingModule } from './legalities-routing.module';
+import { MainNavbarModule } from '../../components/main-navbar/main-navbar.module';
+
 import { LegalitiesPage } from './legalities.page';
+import { TermsComponent } from './terms/terms.component';
+import { PrivacyComponent } from './privacy/privacy.component';
 
 
 @NgModule({
   declarations: [
-    LegalitiesPage
+    LegalitiesPage,
+    TermsComponent,
+    PrivacyComponent
   ],
   imports: [
     CommonModule,
-    LegalitiesRoutingModule
+    LegalitiesRoutingModule,
+    MainNavbarModule
   ]
 })
 export class LegalitiesModule { }
