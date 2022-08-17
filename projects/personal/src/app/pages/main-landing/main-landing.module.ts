139fd8513dd5b854a5f6dc7c01dde11651e28fee
Weps <weaponz326@gmail.com>
2022-08-17 10:56:24 +0000

    main lnading page top ssection

--- projects/personal/src/app/pages/main-landing/main-landing.module.ts
+++ projects/personal/src/app/pages/main-landing/main-landing.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainLandingRoutingModule } from './main-landing-routing.module';
-import { MainLandingPage } from './main-landing.page';
 import { MainNavbarModule } from '../../components/main-navbar/main-navbar.module';
+
+import { MainLandingPage } from './main-landing.page';
+import { MainScrollnavComponent } from './main-scrollnav/main-scrollnav.component';
+import { LandingTopComponent } from './landing-top/landing-top.component';
+import { SuiteLeftComponent } from './suite-left/suite-left.component';
+import { SuiteRightComponent } from './suite-right/suite-right.component';
 
 
 @NgModule({
   declarations: [
-    MainLandingPage
+    MainLandingPage,
+    MainScrollnavComponent,
+    LandingTopComponent,
+    SuiteLeftComponent,
+    SuiteRightComponent
   ],
   imports: [
     CommonModule,
