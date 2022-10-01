4bc2783d585d82b0cad542158d40f5e27439636e
Weps <weaponz326@gmail.com>
2022-10-01 07:26:31 +0000

    school app routing routes

--- projects/school/src/app/app-routing.module.ts
+++ projects/school/src/app/app-routing.module.ts
@@ -49,6 +49,62 @@ const routes: Routes = [
         path: "settings",
         loadChildren: () => import("./pages/modules/settings/settings.module").then(m => m.SettingsModule)
       },
+      {
+        path: "parents",
+        loadChildren: () => import("./pages/modules/parents/parents.module").then(m => m.ParentsModule)
+      },
+      {
+        path: "assessment",
+        loadChildren: () => import("./pages/modules/assessment/assessment.module").then(m => m.AssessmentModule)
+      },
+      {
+        path: "subjects",
+        loadChildren: () => import("./pages/modules/subjects/subjects.module").then(m => m.SubjectsModule)
+      },
+      {
+        path: "attendance",
+        loadChildren: () => import("./pages/modules/attendance/attendance.module").then(m => m.AttendanceModule)
+      },
+      {
+        path: "students",
+        loadChildren: () => import("./pages/modules/students/students.module").then(m => m.StudentsModule)
+      },
+      {
+        path: "lesson-plan",
+        loadChildren: () => import("./pages/modules/lesson-plan/lesson-plan.module").then(m => m.LessonPlanModule)
+      },
+      {
+        path: "reports",
+        loadChildren: () => import("./pages/modules/reports/reports.module").then(m => m.ReportsModule)
+      },
+      {
+        path: "teachers",
+        loadChildren: () => import("./pages/modules/teachers/teachers.module").then(m => m.TeachersModule)
+      },
+      {
+        path: "payments",
+        loadChildren: () => import("./pages/modules/payments/payments.module").then(m => m.PaymentsModule)
+      },
+      {
+        path: "terms",
+        loadChildren: () => import("./pages/modules/terms/terms.module").then(m => m.TermsModule)
+      },
+      {
+        path: "classes",
+        loadChildren: () => import("./pages/modules/classes/classes.module").then(m => m.ClassesModule)
+      },
+      {
+        path: "timetable",
+        loadChildren: () => import("./pages/modules/timetable/timetable.module").then(m => m.TimetableModule)
+      },
+      {
+        path: "fees",
+        loadChildren: () => import("./pages/modules/fees/fees.module").then(m => m.FeesModule)
+      },
+      {
+        path: "sections",
+        loadChildren: () => import("./pages/modules/sections/sections.module").then(m => m.SectionsModule)
+      },
     ]
   }
 ];
