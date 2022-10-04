f22ba41c016946e8fda7d8db159536328b9ed93a
Weps <weaponz326@gmail.com>
2022-10-04 21:45:29 +0000

    menu item and staff code config test with backend

--- projects/restaurant/src/app/models/modules/menu/menu.model.ts
+++ projects/restaurant/src/app/models/modules/menu/menu.model.ts
@@ -1,15 +1,21 @@
 export class MenuGroup {
-    account!: string;
-    menu_group!: string;
-    category!: string;
-  }
-  
-  export class MenuItem {
-    menu_group!: string;
-    item_code!: string;
-    item_name!: string;
-    price!: number;
-    image!: any;
-    description!: string;
-  }
-  
+  account!: string;
+  menu_group!: string;
+  category!: string;
+}
+
+export class MenuItem {
+  menu_group!: string;
+  item_code!: string;
+  item_name!: string;
+  price!: number;
+  image!: any;
+  description!: string;
+}
+
+export class MenuItemCodeConfig {
+  entry_mode!: string;
+  prefix!: string;
+  last_code!: string;
+  suffix!: string;
+}
--- projects/restaurant/src/app/pages/modules/menu/configuration/configuration.component.ts
+++ projects/restaurant/src/app/pages/modules/menu/configuration/configuration.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { ConnectionToastComponent } from 'projects/personal/src/app/components/module-utilities/connection-toast/connection-toast.component'
+
+import { MenuApiService } from 'projects/restaurant/src/app/services/modules-api/menu-api/menu-api.service';
+import { MenuItemCodeConfig } from 'projects/restaurant/src/app/models/modules/menu/menu.model';
+
 
 @Component({
   selector: 'app-configuration',
@@ -7,46 +13,106 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfigurationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private menuApi: MenuApiService) { }
 
   navHeading: any[] = [
     { text: "Configuration", url: "/home/menu/configuration" },
   ];
 
+  @ViewChild('connectionToastComponentReference', { read: ConnectionToastComponent, static: false }) connectionToast!: ConnectionToastComponent;
+
+  isConfigLoading: boolean = false;
+  isConfigSaving: boolean = false;
+
+  configData: any;
+
   entryMode = "Auto";
 
   prefix = "";
-  codeLength = 5;
-  suffix = "MN"
+  codeLength = 0;
+  suffix = ""
 
   sampleID = "";
 
   ngOnInit(): void {
-    this.setSampleId();
+    this.getMenuItemCodeConfig();
+  }
+
+  getMenuItemCodeConfig(){
+    this.isConfigLoading = true;
+
+    this.menuApi.getMenuItemCodeConfig()
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.isConfigLoading = false;
+          this.configData = res;
+
+          this.entryMode = res.entry_mode;
+          this.prefix = res.prefix;
+          this.suffix = res.suffix;
+          this.codeLength = res.last_code.length
+
+          this.setSampleId();
+        },
+        error: (err) => {
+          console.log(err);
+          this.isConfigLoading = false;
+          this.connectionToast.openToast();
+        }
+      })
+  }
+
+  putMenuItemCodeConfig(){
+    let data: MenuItemCodeConfig = {
+      entry_mode: this.entryMode,
+      prefix: this.prefix,
+      last_code: this.configData.last_code.padStart(this.codeLength, "0"),
+      suffix: this.suffix,
+    }
+
+    this.isConfigSaving = true;
+
+    this.menuApi.putMenuItemCodeConfig(data)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.isConfigSaving = false;
+        },
+        error: (err) => {
+          console.log(err);
+          this.isConfigSaving = false;
+          this.connectionToast.openToast();
+        }
+      })
+
+    console.log(data);
   }
 
   entryModeChange(e: any){
     console.log(e.target.value);
 
     if(e.target.value == "Manual"){
-      this.prefix = "";
-      this.codeLength = 0;
-      this.suffix = ""
+      // this.prefix = "";
+      // this.codeLength = 0;
+      // this.suffix = ""
 
       this.sampleID = "";
     }
     else if(e.target.value == "Auto"){
-      this.prefix = "";
-      this.codeLength = 5;
-      this.suffix = "MN"
+      this.prefix = this.configData.prefix;
+      this.codeLength = this.configData.last_code.length;
+      this.suffix = this.configData.suffix
 
       this.setSampleId();
     }
   }
 
   setSampleId(){
-    var code = "1".padStart(this.codeLength, "0");
-    this.sampleID = this.prefix + code + this.suffix;
+    if(this.entryMode == "Auto"){
+      var code = "1".padStart(this.codeLength, "0");
+      this.sampleID = this.prefix + code + this.suffix;
+    }
   }
 
 }
