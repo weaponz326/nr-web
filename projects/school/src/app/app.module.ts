33760de1c06b8e658ed110fcb5cabffaaef194ab
Weps <weaponz326@gmail.com>
2022-10-23 08:11:20 +0000

    .

--- projects/school/src/app/app.module.ts
+++ projects/school/src/app/app.module.ts
@@ -3,23 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { FeesWindowsComponent } from './components/select-windows/fees-windows/fees-windows.component';
-import { SelectFeesComponent } from './components/select-windows/fees-windows/select-fees/select-fees.component';
-import { SelectBillComponent } from './components/select-windows/fees-windows/select-bill/select-bill.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    FeesWindowsComponent,
-    SelectFeesComponent,
-    SelectBillComponent
   ],
   imports: [
     BrowserModule,
@@ -34,7 +28,6 @@ import { SelectBillComponent } from './components/select-windows/fees-windows/se
     }),
     AppRoutingModule,
   ],
-  // providers: [],
   providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
