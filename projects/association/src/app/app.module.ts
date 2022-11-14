789bd310ccbb1d9cfb710e02de35c742e6a61f63
Weps <weaponz326@gmail.com>
2022-11-14 21:35:26 +0000

    added pwa for association project

--- projects/association/src/app/app.module.ts
+++ projects/association/src/app/app.module.ts
@@ -21,6 +21,12 @@ import { environment } from '../environments/environment';
     HttpClientModule,
     RouterModule,
     AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
 
