ab3d5b118f72a3efa8ae301dd53d008f44e54a98
Weps <weaponz326@gmail.com>
2022-11-15 04:01:38 +0000

    started association dues module ui design

--- projects/association/src/app/pages/modules/dues/all-dues/all-dues.component.ts
+++ projects/association/src/app/pages/modules/dues/all-dues/all-dues.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ConnectionToastComponent } from 'projects/personal/src/app/components/module-utilities/connection-toast/connection-toast.component'
+
 
 @Component({
   selector: 'app-all-dues',
@@ -7,9 +11,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllDuesComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
+
+  @ViewChild('connectionToastComponentReference', { read: ConnectionToastComponent, static: false }) connectionToast!: ConnectionToastComponent;
+
+  navHeading: any[] = [
+    { text: "All Dues", url: "/home/dues/all-dues" },
+  ];
+
+  duesGridData: any[] = [];
+
+  isFetchingGridData: boolean =  false;
+  isDataAvailable: boolean =  true;
+
+  currentPage = 0;
+  totalPages = 0;
+  totalItems = 0;
+
+  currentSortColumn = "";
 
   ngOnInit(): void {
+  }
+
+  getAccountDues(page: any, size: any, sortField: any){
+    this.isFetchingGridData = true;
+
+
+  }
+
+  sortTable(column: any){
+    console.log(column);
+    this.getAccountDues(1, 20, column);
+
+    this.currentSortColumn = column;
+  }
+
+  viewDues(duesId: any){
+    console.log(duesId);
+
+    sessionStorage.setItem('association_dues_id', duesId);
+    this.router.navigateByUrl('/home/dues/view-dues');
+  }
+
+
+  onPrint(){
+    console.log("lets start printing...");
+    // this.duesPrint.printAllDues();
   }
 
 }
