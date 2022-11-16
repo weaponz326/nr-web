4b47ddc4f98a2366e92256b66c1a5910131bc3a3
Weps <weaponz326@gmail.com>
2022-11-16 10:08:23 +0000

    association attendance module ui design

--- projects/association/src/app/pages/modules/attendance/view-sheet/view-sheet.component.ts
+++ projects/association/src/app/pages/modules/attendance/view-sheet/view-sheet.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { ConnectionToastComponent } from 'projects/personal/src/app/components/module-utilities/connection-toast/connection-toast.component'
+
 
 @Component({
   selector: 'app-view-sheet',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewSheetComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+  ) { }
+
+  @ViewChild('connectionToastComponentReference', { read: ConnectionToastComponent, static: false }) connectionToast!: ConnectionToastComponent;
+
+  Object = Object;
+
+  attendanceSheetData: any = [];
+
+  isFetchingGridData = false;
+  isSheetSaving = false;
 
   ngOnInit(): void {
+    this.getAttendanceSheet();
+  }
+
+  getAttendanceSheet(){
+    this.isFetchingGridData = true;
+
+    
+  }
+
+  updateAttendanceSheet(){
+    let data = { sheet: this.attendanceSheetData };
+
+    this.isSheetSaving = true;
+
+    
+  }
+
+  onCheckChange(event: any, index: any, key: any){
+    console.log(event.target.checked);
+    this.attendanceSheetData[index].checks[key] = event.target.checked;
+    console.log(this.attendanceSheetData);
+  }
+
+  stringToDate(string: string){
+    return new Date(string);
   }
 
 }
