f1fde03061275b7c35b86c0261536cebeff02cc7
Weps <weaponz326@gmail.com>
2022-12-14 07:54:03 +0000

    enterprise budgget module ui design

--- projects/enterprise/src/app/pages/modules/budget/all-budget/all-budget.component.ts
+++ projects/enterprise/src/app/pages/modules/budget/all-budget/all-budget.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { NewBudgetComponent } from '../new-budget/new-budget.component';
+import { ConnectionToastComponent } from 'projects/personal/src/app/components/module-utilities/connection-toast/connection-toast.component';
+
+// import { BudgetApiService } from 'projects/enterprise/src/app/services/modules-api/budget-api/budget-api.service';
+// import { BudgetPrintService } from 'projects/enterprise/src/app/services/modules-printing/budget-print/budget-print.service';
+
 
 @Component({
   selector: 'app-all-budget',
@@ -7,9 +15,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllBudgetComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    // private budgetApi: BudgetApiService,
+    // private budgetPrint: BudgetPrintService
+  ) { }
+
+  @ViewChild('newBudgetComponentReference', { read: NewBudgetComponent, static: false }) newBudget!: NewBudgetComponent;
+  @ViewChild('connectionToastComponentReference', { read: ConnectionToastComponent, static: false }) connectionToast!: ConnectionToastComponent;
+
+  navHeading: any[] = [
+    { text: "All Budgets", url: "/home/budget/all-budget" },
+  ];
+
+  budgetGridData: any[] = [];
+
+  isFetchingGridData: boolean =  false;
+  isDataAvailable: boolean =  true;
+
+  currentPage = 0;
+  totalPages = 0;
+  totalItems = 0;
+
+  currentSortColumn = "";
 
   ngOnInit(): void {
+    this.getUserBudget(1,20, "");
+  }
+
+  getUserBudget(page: any, size: any, sortField: any){
+    this.isFetchingGridData =  true;
+
+    // this.budgetApi.getUserBudgets(page, size, sortField)
+    //   .subscribe({
+    //     next: (res) => {
+    //       console.log(res);
+    //       this.budgetGridData = res.results;
+    //       this.currentPage = res.current_page;
+    //       this.totalPages = res.total_pages;
+    //       this.totalItems = res.count;
+
+    //       this.isFetchingGridData = false;
+    //       if(this.totalItems == 0)
+    //         this.isDataAvailable = false
+    //     },
+    //     error: (err) => {
+    //       this.connectionToast.openToast();
+    //       this.isFetchingGridData = false;
+    //       console.log(err);
+    //     }
+    //   })
+  }
+
+  sortTable(column: any){
+    console.log(column);
+    this.getUserBudget(1, 20, column);
+
+    this.currentSortColumn = column;
+  }
+
+  viewBudget(budgetId: any){
+    console.log(budgetId);
+
+    sessionStorage.setItem("personal_budget_id", budgetId);
+    this.router.navigateByUrl("/home/budget/view-budget");
+  }
+
+  onPrint(){
+    console.log("lets start printing...");
+    // this.budgetPrint.printAllBudget();
   }
 
 }
--- projects/enterprise/src/app/pages/modules/budget/edit-expenditure/edit-expenditure.component.ts
+++ projects/enterprise/src/app/pages/modules/budget/edit-expenditure/edit-expenditure.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+// import { Expenditure } from 'projects/enterprise/src/app/models/modules/budget/budget.model';
+
 
 @Component({
   selector: 'app-edit-expenditure',
@@ -9,7 +13,50 @@ export class EditExpenditureComponent implements OnInit {
 
   constructor() { }
 
+  @Output() saveExpenditureEvent = new EventEmitter<any>();
+
+  @ViewChild('editButtonElementReference', { read: ElementRef, static: false }) editButton!: ElementRef;
+  @ViewChild('dismissButtonElementReference', { read: ElementRef, static: false }) dismissButton!: ElementRef;
+
+  expenditureFormData: any;
+
+  isSaving = false;
+  budgetData: any;
+
+  editExpenditureForm = new FormGroup({
+    itemNumber: new FormControl({value: '', disabled: true}),
+    itemDescription: new FormControl(''),
+    amount: new FormControl(0)
+  })
+
   ngOnInit(): void {
+  }
+
+  openModal(expenditure: any){
+    this.expenditureFormData = expenditure;
+
+    this.editExpenditureForm.controls.itemNumber.setValue(this.expenditureFormData.item_number);
+    this.editExpenditureForm.controls.itemDescription.setValue(this.expenditureFormData.item_description);
+    this.editExpenditureForm.controls.amount.setValue(this.expenditureFormData.amount);
+
+    this.editButton.nativeElement.click();
+  }
+
+  saveExpenditure(){
+    // let data: Expenditure = {
+    let data = {
+      item_number: this.editExpenditureForm.controls.itemNumber.value as string,
+      item_description: this.editExpenditureForm.controls.itemDescription.value as string,
+      amount: this.editExpenditureForm.controls.amount.value as number,
+      budget: sessionStorage.getItem('enterprise_budget_id') as string,
+    }
+
+    let expenditure = {
+      id: this.expenditureFormData.id,
+      data: data
+    }
+
+    this.saveExpenditureEvent.emit(expenditure);
   }
 
 }
