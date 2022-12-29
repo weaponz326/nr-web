ff5f5ddd72f916f95415bef8504ba2dfb21e8144
Weps <weaponz326@gmail.com>
2022-12-29 04:20:52 +0000

    association executives module test

--- projects/association/src/app/pages/modules/executives/all-executives/all-executives.component.ts
+++ projects/association/src/app/pages/modules/executives/all-executives/all-executives.component.ts
@@ -3,6 +3,9 @@ import { Router } from '@angular/router';
 
 import { ConnectionToastComponent } from 'projects/personal/src/app/components/module-utilities/connection-toast/connection-toast.component'
 
+import { ExecutivesApiService } from 'projects/association/src/app/services/modules-api/executives-api/executives-api.service';
+// import { ExecutivesPrintService } from 'projects/association/src/app/services/modules-printing/executives-print/executives-print.service';
+
 
 @Component({
   selector: 'app-all-executives',
@@ -13,6 +16,8 @@ export class AllExecutivesComponent implements OnInit {
 
   constructor(
     private router: Router,
+    private executivesApi: ExecutivesApiService,
+    // private executivesPrint: ExecutivesPrintService,
   ) { }
 
   @ViewChild('connectionToastComponentReference', { read: ConnectionToastComponent, static: false }) connectionToast!: ConnectionToastComponent;
@@ -33,12 +38,32 @@ export class AllExecutivesComponent implements OnInit {
   currentSortColumn = "";
 
   ngOnInit(): void {
+    this.getAccountExecutive(1, 20, "-created_at");
   }
 
   getAccountExecutive(page: any, size: any, sortField: any){
     this.isFetchingGridData = true;
 
+    this.executivesApi.getAccountExecutive(page, size, sortField)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.executivesGridData = res.results;
 
+          this.currentPage = res.current_page;
+          this.totalPages = res.total_pages;
+          this.totalItems = res.count;
+
+          this.isFetchingGridData = false;
+          if(this.totalItems == 0)
+            this.isDataAvailable = false          
+        },
+        error: (err) => {
+          console.log(err);
+          this.isFetchingGridData = false;
+          this.connectionToast.openToast();
+        }
+      })
   }
 
   sortTable(column: any){
--- projects/association/src/app/pages/modules/executives/view-executive/view-executive.component.ts
+++ projects/association/src/app/pages/modules/executives/view-executive/view-executive.component.ts
@@ -6,6 +6,10 @@ import { ConnectionToastComponent } from 'projects/personal/src/app/components/m
 import { DeleteModalOneComponent } from 'projects/personal/src/app/components/module-utilities/delete-modal-one/delete-modal-one.component'
 
 import { CustomCookieService } from 'projects/application/src/app/services/custom-cookie/custom-cookie.service';
+import { ExecutivesApiService } from 'projects/association/src/app/services/modules-api/executives-api/executives-api.service';
+// import { ExecutivesPrintService } from 'projects/association/src/app/services/modules-printing/executives-print/executives-print.service';
+
+import { Executive } from 'projects/association/src/app/models/modules/executives/executives.model';
 
 
 @Component({
@@ -18,6 +22,8 @@ export class ViewExecutiveComponent implements OnInit {
   constructor(
     private router: Router,
     private customCookie: CustomCookieService,
+    private executivesApi: ExecutivesApiService,
+    // private executivesPrint: ExecutivesPrintService,
   ) { }
 
   @ViewChild('executiveFormComponentReference', { read: ExecutiveFormComponent, static: false }) executiveForm!: ExecutiveFormComponent;
@@ -42,35 +48,47 @@ export class ViewExecutiveComponent implements OnInit {
   getExecutive(){
     this.isExecutiveLoading = true;
 
-    // this.executivesApi.getExecutive()
-    //   .subscribe({
-    //     next: (res) => {
-    //       console.log(res);
-    //       this.executiveData = res;
-    //       this.isExecutiveLoading = false;
+    this.executivesApi.getExecutive()
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.executiveData = res;
+          this.isExecutiveLoading = false;
 
-    //       this.executiveForm.executiveForm.controls.position.setValue(this.executiveData.position);
-    //     },
-    //     error: (err) => {
-    //       console.log(err);
-    //       this.isExecutiveLoading = false;
-    //       this.connectionToast.openToast();
-    //     }
-    //   })
+          this.executiveForm.executiveForm.controls.position.setValue(this.executiveData.position);
+        },
+        error: (err) => {
+          console.log(err);
+          this.isExecutiveLoading = false;
+          this.connectionToast.openToast();
+        }
+      })
   }
 
   putExecutive(){
     console.log('u are saving a new executive');
 
+    // var data: Executive = {
     var data = {
       account: this.customCookie.getCookie('association_id') as string,
-      position: this.executiveForm.executiveForm.controls.position.value,
+      position: this.executiveForm.executiveForm.controls.position.value as string,
     }
 
     console.log(data);
     this.isExecutiveSaving = true;
 
-    
+    this.executivesApi.putExecutive(data)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.isExecutiveSaving = false;
+        },
+        error: (err) => {
+          console.log(err);
+          this.isExecutiveSaving = false;
+          this.connectionToast.openToast();
+        }
+      })
   }
 
   confirmDelete(){
@@ -80,8 +98,17 @@ export class ViewExecutiveComponent implements OnInit {
   deleteExecutive(){
     this.isExecutiveDeleting = true;
 
-    
-    
+    this.executivesApi.deleteExecutive()
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.router.navigateByUrl('/home/executives/all-executives');
+        },
+        error: (err) => {
+          console.log(err);
+          this.connectionToast.openToast();
+        }
+      })    
   }
 
   onPrint(){
