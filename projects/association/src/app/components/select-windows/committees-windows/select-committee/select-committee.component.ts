152b64a08681253bec164d20dfbb03f100a8eaf7
Weps <weaponz326@gmail.com>
2022-12-29 22:10:01 +0000

    association select components

--- projects/association/src/app/components/select-windows/committees-windows/select-committee/select-committee.component.ts
+++ projects/association/src/app/components/select-windows/committees-windows/select-committee/select-committee.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+
+import { ConnectionToastComponent } from 'projects/personal/src/app/components/module-utilities/connection-toast/connection-toast.component';
+
+import { CommitteesApiService } from 'projects/association/src/app/services/modules-api/committees-api/committees-api.service';
+
 
 @Component({
   selector: 'app-select-committee',
@@ -7,9 +12,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectCommitteeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private committeesApi: CommitteesApiService) { }
+
+  @Output() rowSelected = new EventEmitter<object>();
+  @Input() closeTarget = "";
+
+  @ViewChild('openButtonElementReference', { read: ElementRef, static: false }) openButton!: ElementRef;
+  @ViewChild('closeButtonElementReference', { read: ElementRef, static: false }) closeButton!: ElementRef;
+
+  @ViewChild('connectionToastComponentReference', { read: ConnectionToastComponent, static: false }) connectionToast!: ConnectionToastComponent;
+
+  committeesGridData: any[] = [];
+
+  isFetchingGridData: boolean =  false;
+  isDataAvailable: boolean =  true;
+
+  currentPage = 0;
+  totalPages = 0;
+  totalItems = 0;
+
+  currentSortColumn = "";
 
   ngOnInit(): void {
+  }
+
+  openModal(){
+    this.committeesGridData = [];
+    this.getAccountCommittee(1, 20, "-created_at");
+    this.openButton.nativeElement.click();
+  }
+
+  getAccountCommittee(page: any, size: any, sortField: any){
+    this.isFetchingGridData = true;
+
+    this.committeesApi.getAccountCommittee(page, size, sortField)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.committeesGridData = res.results;
+
+          this.currentPage = res.current_page;
+          this.totalPages = res.total_pages;
+          this.totalItems = res.count;
+
+          this.isFetchingGridData = false;
+          if(this.totalItems == 0)
+            this.isDataAvailable = false          
+        },
+        error: (err) => {
+          console.log(err);
+          this.isFetchingGridData = false;
+          this.connectionToast.openToast();
+        }
+      })
+  }
+
+  sortTable(column: any){
+    console.log(column);
+    this.getAccountCommittee(1, 20, column);
+
+    this.currentSortColumn = column;
+  }
+
+  selectRow(row: any){
+    this.rowSelected.emit(row);
+    this.closeButton.nativeElement.click();
+    console.log(row);
   }
 
 }
