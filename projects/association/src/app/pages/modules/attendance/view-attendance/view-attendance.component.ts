84dc379bc003544860f84a109ce85a1440d621fa
Weps <weaponz326@gmail.com>
2022-12-29 08:16:40 +0000

    association attendance module testing

--- projects/association/src/app/pages/modules/attendance/view-attendance/view-attendance.component.ts
+++ projects/association/src/app/pages/modules/attendance/view-attendance/view-attendance.component.ts
@@ -6,6 +6,9 @@ import { ConnectionToastComponent } from 'projects/personal/src/app/components/m
 import { DeleteModalOneComponent } from 'projects/personal/src/app/components/module-utilities/delete-modal-one/delete-modal-one.component'
 
 import { CustomCookieService } from 'projects/application/src/app/services/custom-cookie/custom-cookie.service';
+import { AttendanceApiService } from 'projects/association/src/app/services/modules-api/attendance-api/attendance-api.service';
+
+import { Attendance } from 'projects/association/src/app/models/modules/attendance/attendance.model';
 
 
 @Component({
@@ -18,6 +21,7 @@ export class ViewAttendanceComponent implements OnInit {
   constructor(
     private router: Router,
     private customCookie: CustomCookieService,
+    private attendanceApi: AttendanceApiService
   ) { }
 
   @ViewChild('attendanceFormComponentReference', { read: AttendanceFormComponent, static: false }) attendanceForm!: AttendanceFormComponent;
@@ -48,29 +52,69 @@ export class ViewAttendanceComponent implements OnInit {
   getAttendance(){
     this.isAttendanceLoading = true;
 
-    
+    this.attendanceApi.getAttendance()
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.attendanceFormData = res;
+          this.isAttendanceLoading = false;
+
+          this.attendanceForm.attendanceForm.controls.attendanceCode.setValue(this.attendanceFormData.attendance_code);
+          this.attendanceForm.attendanceForm.controls.attendanceName.setValue(this.attendanceFormData.attendance_name);
+          this.attendanceForm.attendanceForm.controls.fromDate.setValue(this.attendanceFormData.from_date);
+          this.attendanceForm.attendanceForm.controls.toDate.setValue(this.attendanceFormData.to_date);
+          this.attendanceForm.attendanceForm.controls.fiscalYear.setValue(this.attendanceFormData.fiscal_year);
+
+        },
+        error: (err) => {
+          console.log(err);
+          this.isAttendanceLoading = false;
+          this.connectionToast.openToast();
+        }
+      })
   }
 
   putAttendance(){
-    let data = {
+    let data: Attendance = {
       account: this.customCookie.getCookie('association_id') as string,
       attendance_code: this.attendanceForm.attendanceForm.controls.attendanceCode.value as string,
       attendance_name: this.attendanceForm.attendanceForm.controls.attendanceName.value as string,
-      from_date: this.attendanceForm.attendanceForm.controls.fromDate.value as string,
-      to_date: this.attendanceForm.attendanceForm.controls.toDate.value as string,
-      fiscal_year: this.attendanceForm.selectedYearId,        
+      from_date: this.attendanceForm.attendanceForm.controls.fromDate.value,
+      to_date: this.attendanceForm.attendanceForm.controls.toDate.value,
+      // fiscal_year: this.attendanceForm.selectedYearId,        
     }
 
     console.log(data);
     this.isAttendanceSaving = true;
 
-    
+    this.attendanceApi.putAttendance(data)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.isAttendanceSaving = false;
+        },
+        error: (err) => {
+          console.log(err);
+          this.isAttendanceSaving = false;
+          this.connectionToast.openToast();
+        }
+      })
   }
 
   deleteAttendance(){
     this.isAttendanceDeleting = true;
 
-    
+    this.attendanceApi.deleteAttendance()
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.router.navigateByUrl('/home/attendance/all-attendance');
+        },
+        error: (err) => {
+          console.log(err);
+          this.connectionToast.openToast();
+        }
+      })    
   }
 
   // printing
