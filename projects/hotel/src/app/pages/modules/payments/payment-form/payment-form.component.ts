000a1d0da10a7365054bd75c385eaadc500c6a58
Weps <weaponz326@gmail.com>
2023-01-14 07:47:34 +0000

    hotel payments module ui design

--- projects/hotel/src/app/pages/modules/payments/payment-form/payment-form.component.ts
+++ projects/hotel/src/app/pages/modules/payments/payment-form/payment-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-payment-form',
@@ -9,7 +11,27 @@ export class PaymentFormComponent implements OnInit {
 
   constructor() { }
 
+  paymentForm = new FormGroup({
+    paymentCode: new FormControl(''),
+    paymentDate: new FormControl(),
+    guestCode: new FormControl({value: '', disabled: true}),
+    guestName: new FormControl({value: '', disabled: true}),
+    billCode: new FormControl({value: 0, disabled: true}),
+    totalAmount: new FormControl({value: 0, disabled: true}),
+    amountPaid: new FormControl(0),
+    balance: new FormControl({value: 0, disabled: true}),
+  })
+
   ngOnInit(): void {
+  }
+
+  setBalance(){
+    let amountPaid = this.paymentForm.controls.amountPaid.value;
+    let totalAmount = this.paymentForm.controls.totalAmount.value;
+
+    if(totalAmount != null && amountPaid != null){
+      this.paymentForm.controls.balance.setValue(totalAmount - amountPaid);
+    }
   }
 
 }
