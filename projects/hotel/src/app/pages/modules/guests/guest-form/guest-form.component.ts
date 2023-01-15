46ffe07280676aa09429e0e1d1e4910461c283dd
Weps <weaponz326@gmail.com>
2023-01-15 05:39:40 +0000

    hotel guests module ui design

--- projects/hotel/src/app/pages/modules/guests/guest-form/guest-form.component.ts
+++ projects/hotel/src/app/pages/modules/guests/guest-form/guest-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-guest-form',
@@ -9,6 +11,17 @@ export class GuestFormComponent implements OnInit {
 
   constructor() { }
 
+  guestForm = new FormGroup({
+    guestCode: new FormControl(''),
+    guestName: new FormControl(''),
+    guestType: new FormControl(''),
+    phone: new FormControl(''),
+    email: new FormControl(''),
+    address: new FormControl(''),
+    state: new FormControl(''),
+    city: new FormControl(''),
+  })
+  
   ngOnInit(): void {
   }
 
