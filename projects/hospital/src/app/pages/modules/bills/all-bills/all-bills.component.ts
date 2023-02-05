49c3c0a19e7268b41d5f4d2c0b91bbbd4d65a794
Weps <weaponz326@gmail.com>
2023-02-05 19:50:41 +0000

    started hospital bills module ui design

--- projects/hospital/src/app/pages/modules/bills/all-bills/all-bills.component.ts
+++ projects/hospital/src/app/pages/modules/bills/all-bills/all-bills.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { NewBillComponent } from '../new-bill/new-bill.component'
+import { ConnectionToastComponent } from 'projects/personal/src/app/components/module-utilities/connection-toast/connection-toast.component'
+
+// import { BillsApiService } from 'projects/hospital/src/app/services/modules-api/bills-api/bills-api.service';
+// import { BillsPrintService } from 'projects/hospital/src/app/services/modules-printing/bills-print/bills-print.service';
+
 
 @Component({
   selector: 'app-all-bills',
@@ -7,9 +15,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllBillsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    // private billsApi: BillsApiService,
+    // private billsPrint: BillsPrintService
+  ) { }
+
+  @ViewChild('newBillComponentReference', { read: NewBillComponent, static: false }) newBill!: NewBillComponent;
+  @ViewChild('connectionToastComponentReference', { read: ConnectionToastComponent, static: false }) connectionToast!: ConnectionToastComponent;
+
+  navHeading: any[] = [
+    { text: "All Bills", url: "/home/bills/all-bills" },
+  ];
+
+  billsGridData: any[] = [];
+
+  isFetchingGridData: boolean =  false;
+  isDataAvailable: boolean =  true;
+
+  currentPage = 0;
+  totalPages = 0;
+  totalItems = 0;
+
+  currentSortColumn = "";
 
   ngOnInit(): void {
+    this.getAccountBill(1, 20, "-created_at");
+  }
+
+  getAccountBill(page: any, size: any, sortField: any){
+    this.isFetchingGridData = true;
+
+    // this.billsApi.getAccountBill(page, size, sortField)
+    //   .subscribe({
+    //     next: (res) => {
+    //       console.log(res);
+    //       this.billsGridData = res.results;
+
+    //       this.currentPage = res.current_page;
+    //       this.totalPages = res.total_pages;
+    //       this.totalItems = res.count;
+
+    //       this.isFetchingGridData = false;
+    //       if(this.totalItems == 0)
+    //         this.isDataAvailable = false
+    //     },
+    //     error: (err) => {
+    //       console.log(err);
+    //       this.isFetchingGridData = false;
+    //       this.connectionToast.openToast();
+    //     }
+    //   })
+  }
+
+  sortTable(column: any){
+    console.log(column);
+    this.getAccountBill(1, 20, column);
+
+    this.currentSortColumn = column;
+  }
+
+  viewBill(billId: any){
+    console.log(billId);
+
+    sessionStorage.setItem("hospital_bill_id", billId);
+    this.router.navigateByUrl("/home/bills/view-bill");
+  }
+
+  onPrint(){
+    console.log("lets start printing...");
+    // this.billsPrint.printAllBills();
   }
 
 }
--- projects/hospital/src/app/pages/modules/bills/new-bill/new-bill.component.ts
+++ projects/hospital/src/app/pages/modules/bills/new-bill/new-bill.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup } from '@angular/forms';
+
+import { ConnectionToastComponent } from 'projects/personal/src/app/components/module-utilities/connection-toast/connection-toast.component'
+
+import { CustomCookieService } from 'projects/application/src/app/services/custom-cookie/custom-cookie.service';
+// import { BillsApiService } from 'projects/hospital/src/app/services/modules-api/bills-api/bills-api.service';
+
+// import { Bill } from 'projects/hospital/src/app/models/modules/bills/bills.model';
+
 
 @Component({
   selector: 'app-new-bill',
@@ -7,9 +17,90 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewBillComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private customCookie: CustomCookieService,
+    // private billsApi: BillsApiService,
+  ) { }
+
+  @ViewChild('newButtonElementReference', { read: ElementRef, static: false }) newButton!: ElementRef;
+  @ViewChild('dismissButtonElementReference', { read: ElementRef, static: false }) dismissButton!: ElementRef;
+  @ViewChild('connectionToastComponentReference', { read: ConnectionToastComponent, static: false }) connectionToast!: ConnectionToastComponent;
+
+  isBillSaving = false;
+
+  billForm = new FormGroup({
+    billCode: new FormControl(''),
+    billDate: new FormControl(),
+    patientName: new FormControl(''),
+    patientNumber: new FormControl(''),
+    admissionCode: new FormControl(''),
+  })
+
 
   ngOnInit(): void {
+  }
+
+  openModal(){
+    this.newButton.nativeElement.click();
+    this.billForm.controls.billDate.setValue(new Date().toISOString().slice(0, 16));
+    this.getNewBillCodeConfig();
+  }
+
+  createBill(){
+    // let data: Bill = {
+    let data = {
+      account: this.customCookie.getCookie('hospital_id') as string,
+      bill_code: this.billForm.controls.billCode.value as string,
+      bill_date: this.billForm.controls.billDate.value as string,
+      total_amount: 0,
+      bill_status: "",
+    }
+
+    console.log(data);
+    this.isBillSaving = true;
+
+    // this.billsApi.postBill(data)
+    //   .subscribe({
+    //     next: (res) => {
+    //       console.log(res);
+
+    //       if(res.id){
+    //         sessionStorage.setItem('hospital_bill_id', res.id);
+
+    //         this.dismissButton.nativeElement.click();
+    //         this.isBillSaving = false;
+    //         this.router.navigateByUrl('/home/bills/view-bill');
+    //       }
+    //     },
+    //     error: (err) => {
+    //       console.log(err);
+    //       this.isBillSaving = false;
+    //       this.connectionToast.openToast();
+    //     }
+    //   })
+
+    console.log(data);
+  }
+
+  getNewBillCodeConfig(){
+    this.billForm.controls.billCode.disable();
+
+    // this.billsApi.getNewBillCodeConfig()
+    //   .subscribe({
+    //     next: (res) => {
+    //       console.log(res);
+
+    //       if(res.code)
+    //         this.billForm.controls.billCode.setValue(res.code);
+    //       else
+    //         this.billForm.controls.billCode.enable();
+    //     },
+    //     error: (err) => {
+    //       console.log(err);
+    //       this.connectionToast.openToast();
+    //     }
+    //   })
   }
 
 }
