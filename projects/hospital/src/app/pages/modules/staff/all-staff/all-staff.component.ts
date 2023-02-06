c007ddcd2e16706bea141be63f5e726253d0e134
Weps <weaponz326@gmail.com>
2023-02-06 22:27:02 +0000

    hospital staff module ui design

--- projects/hospital/src/app/pages/modules/staff/all-staff/all-staff.component.ts
+++ projects/hospital/src/app/pages/modules/staff/all-staff/all-staff.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ConnectionToastComponent } from 'projects/personal/src/app/components/module-utilities/connection-toast/connection-toast.component'
+
+// import { StaffApiService } from 'projects/hospital/src/app/services/modules-api/staff-api/staff-api.service';
+// import { StaffPrintService } from 'projects/hospital/src/app/services/modules-printing/staff-print/staff-print.service';
+
 
 @Component({
   selector: 'app-all-staff',
@@ -7,9 +14,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllStaffComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    // private staffApi: StaffApiService,
+    // private staffPrint: StaffPrintService,
+  ) { }
+
+  @ViewChild('connectionToastComponentReference', { read: ConnectionToastComponent, static: false }) connectionToast!: ConnectionToastComponent;
+
+  navHeading: any[] = [
+    { text: "All Staff", url: "/home/staff/all-staff" },
+  ];
+
+  staffGridData: any[] = [];
+
+  isFetchingGridData: boolean =  false;
+  isDataAvailable: boolean =  true;
+
+  currentPage = 0;
+  totalPages = 0;
+  totalItems = 0;
+
+  currentSortColumn = "";
 
   ngOnInit(): void {
+    this.getAccountStaff(1, 20, "-created_at");
+  }
+
+  getAccountStaff(page: any, size: any, sortField: any){
+    this.isFetchingGridData = true;
+
+    // this.staffApi.getAccountStaff(page, size, sortField)
+    //   .subscribe({
+    //     next: (res) => {
+    //       console.log(res);
+    //       this.staffGridData = res.results;
+
+    //       this.currentPage = res.current_page;
+    //       this.totalPages = res.total_pages;
+    //       this.totalItems = res.count;
+
+    //       this.isFetchingGridData = false;
+    //       if(this.totalItems == 0)
+    //         this.isDataAvailable = false
+    //     },
+    //     error: (err) => {
+    //       console.log(err);
+    //       this.isFetchingGridData = false;
+    //       this.connectionToast.openToast();
+    //     }
+    //   })
+  }
+
+  sortTable(column: any){
+    console.log(column);
+    this.getAccountStaff(1, 20, column);
+
+    this.currentSortColumn = column;
+  }
+
+  viewStaff(staffId: any){
+    console.log(staffId);
+
+    sessionStorage.setItem('hospital_staff_id', staffId);
+    this.router.navigateByUrl('/home/staff/view-staff');
+  }
+
+  onPrint(){
+    console.log("lets start printing...");
+    // this.staffPrint.printAllStaff();
   }
 
 }
--- projects/hospital/src/app/pages/modules/staff/new-staff/new-staff.component.ts
+++ projects/hospital/src/app/pages/modules/staff/new-staff/new-staff.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { StaffFormComponent } from '../staff-form/staff-form.component';
+import { ConnectionToastComponent } from 'projects/personal/src/app/components/module-utilities/connection-toast/connection-toast.component'
+
+import { CustomCookieService } from 'projects/application/src/app/services/custom-cookie/custom-cookie.service';
+// import { StaffApiService } from 'projects/hospital/src/app/services/modules-api/staff-api/staff-api.service';
+
+// import { Staff } from 'projects/hospital/src/app/models/modules/staff/staff.model';
+
 
 @Component({
   selector: 'app-new-staff',
@@ -7,9 +17,106 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewStaffComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private customCookie: CustomCookieService,
+    // private staffApi: StaffApiService
+  ) { }
+
+  @ViewChild('staffFormComponentReference', { read: StaffFormComponent, static: false }) staffForm!: StaffFormComponent;
+  @ViewChild('connectionToastComponentReference', { read: ConnectionToastComponent, static: false }) connectionToast!: ConnectionToastComponent;
+
+  navHeading: any[] = [
+    { text: "New Staff", url: "/home/staff/new-staff" },
+  ];
+
+  isStaffSaving = false;
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.getNewStaffCodeConfig();
+  }
+
+  createStaff(){
+    console.log('u are saving a new staff');
+
+    var data = {
+      account: this.customCookie.getCookie('hospital_id') as string,
+      first_name: this.staffForm.staffForm.controls.firstName.value as string,
+      last_name: this.staffForm.staffForm.controls.lastName.value as string,
+      sex: this.staffForm.staffForm.controls.sex.value as string,
+      date_of_birth: this.staffForm.bday.getValue(),
+      nationality: this.staffForm.staffForm.controls.nationality.value as string,
+      religion: this.staffForm.staffForm.controls.religion.value as string,
+      phone: this.staffForm.staffForm.controls.phone.value as string,
+      email: this.staffForm.staffForm.controls.email.value as string,
+      address: this.staffForm.staffForm.controls.address.value as string,
+      state: this.staffForm.staffForm.controls.state.value as string,
+      city: this.staffForm.staffForm.controls.city.value as string,
+      post_code: this.staffForm.staffForm.controls.postCode.value as string,
+      staff_code: this.staffForm.staffForm.controls.staffCode.value as string,
+      department: this.staffForm.staffForm.controls.department.value as string,
+      job: this.staffForm.staffForm.controls.job.value as string,
+    }
+
+    console.log(data);
+    this.isStaffSaving = true;
+
+    // this.staffApi.postStaff(data)
+    //   .subscribe({
+    //     next: (res) => {
+    //       console.log(res);
+    //       sessionStorage.setItem('hospital_staff_id', res.id);
+
+    //       if(this.staffForm.photo.isImageChanged){
+    //         this.putStaffImage();
+    //       }
+    //       else{
+    //         this.router.navigateByUrl('/home/staff/view-staff');                    
+    //       }
+    //     },
+    //     error: (err) => {
+    //       console.log(err);
+    //       this.isStaffSaving = false;
+    //       this.connectionToast.openToast();
+    //     }
+    //   })
+  }
+
+  putStaffImage(){
+    // this.staffApi.putStaffPhoto(this.staffForm.photo.image)
+    //   .subscribe({
+    //     next: (res) => {
+    //       console.log(res);
+    //       this.router.navigateByUrl('/home/staff/view-staff');                    
+    //     },
+    //     error: (err) => {
+    //       console.log(err);          
+    //       this.connectionToast.openToast();
+    //     }
+    //   })
+  }
+
+  getNewStaffCodeConfig(){
+    this.staffForm.staffForm.controls.staffCode.disable();
+
+    // this.staffApi.getNewStaffCodeConfig()
+    //   .subscribe({
+    //     next: (res) => {
+    //       console.log(res);
+
+    //       if(res.code)
+    //         this.staffForm.staffForm.controls.staffCode.setValue(res.code);
+    //       else
+    //         this.staffForm.staffForm.controls.staffCode.enable();
+    //     },
+    //     error: (err) => {
+    //       console.log(err);
+    //       this.connectionToast.openToast();
+    //     }
+    //   })
   }
 
 }
