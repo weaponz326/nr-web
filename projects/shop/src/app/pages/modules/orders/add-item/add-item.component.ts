65e756fe333b71aeb45b5af03653ebe2ce8decab
Weps <weaponz326@gmail.com>
2023-03-03 12:26:09 +0000

    shop orders module ui design

--- projects/shop/src/app/pages/modules/orders/add-item/add-item.component.ts
+++ projects/shop/src/app/pages/modules/orders/add-item/add-item.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Output, EventEmitter, ElementRef } from '@angular/core';
+
+import { ItemFormComponent } from '../item-form/item-form.component'
+// import { SelectProductComponent } from '../../../../components/select-windows/menu-windows/select-product/select-product.component';
+
+// import { OrderItem } from 'projects/shop/src/app/models/modules/orders/orders.model';
+
 
 @Component({
   selector: 'app-add-item',
@@ -9,7 +15,63 @@ export class AddItemComponent implements OnInit {
 
   constructor() { }
 
+  @Output() saveItemEvent = new EventEmitter<any>();
+
+  @ViewChild('addButtonElementReference', { read: ElementRef, static: false }) addButton!: ElementRef;
+  @ViewChild('dismissButtonElementReference', { read: ElementRef, static: false }) dismissButton!: ElementRef;
+
+  @ViewChild('itemFormComponentReference', { read: ItemFormComponent, static: false }) itemForm!: ItemFormComponent;
+  // @ViewChild('selectProductComponentReference', { read: SelectProductComponent, static: false }) selectProduct!: SelectProductComponent;
+
+  isItemSaving = false;
+
   ngOnInit(): void {
+  }
+
+  openModal(lastId: any){
+    this.itemForm.itemForm.controls.itemNumber.setValue(lastId + 1);
+    this.itemForm.itemForm.controls.price.setValue(0.00);
+    this.itemForm.itemForm.controls.quantity.setValue(1);
+
+    this.addButton.nativeElement.click();
+  }
+
+  saveItem(){
+    // let data: OrderItem = {
+    let data = {
+      item_number: this.itemForm.itemForm.controls.itemNumber.value as number,
+      order: sessionStorage.getItem('shop_order_id') as string,
+      quantity: this.itemForm.itemForm.controls.quantity.value as number,
+      product: this.itemForm.selectedProductId,
+    }
+
+    if(this.itemForm.selectedProductId != "")
+      this.saveItemEvent.emit(data);
+  }
+
+  resetForm(){
+    this.itemForm.itemForm.controls.itemNumber.setValue(null);
+    this.itemForm.itemForm.controls.productCode.setValue('');
+    this.itemForm.itemForm.controls.productName.setValue('');
+    this.itemForm.itemForm.controls.price.setValue(0.00);
+    this.itemForm.itemForm.controls.quantity.setValue(1);
+    this.itemForm.selectedProductId = "";
+    this.itemForm.selectedProductData = null;
+  }
+
+  openProductWindow(){
+    console.log("You are opening select menu item window")
+    // this.selectProduct.openModal();
+  }
+
+  onProductSelected(itemData: any){
+    console.log(itemData);
+    this.itemForm.selectedProductId = itemData.id;
+    this.itemForm.selectedProductData = itemData;
+
+    this.itemForm.itemForm.controls.productCode.setValue(itemData?.product?.product_code);
+    this.itemForm.itemForm.controls.productName.setValue(itemData?.product?.product_name);
+    this.itemForm.itemForm.controls.price.setValue(itemData.price);
   }
 
 }
--- projects/shop/src/app/pages/modules/orders/edit-item/edit-item.component.ts
+++ projects/shop/src/app/pages/modules/orders/edit-item/edit-item.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Output, EventEmitter, ElementRef } from '@angular/core';
+
+import { ItemFormComponent } from '../item-form/item-form.component'
+// import { SelectProductComponent } from '../../../../components/select-windows/menu-windows/select-product/select-product.component';
+
+// import { OrderItem } from 'projects/shop/src/app/models/modules/orders/orders.model';
+
 
 @Component({
   selector: 'app-edit-item',
@@ -9,7 +15,65 @@ export class EditItemComponent implements OnInit {
 
   constructor() { }
 
+  @Output() saveItemEvent = new EventEmitter<any>();
+
+  @ViewChild('editButtonElementReference', { read: ElementRef, static: false }) editButton!: ElementRef;
+  @ViewChild('dismissButtonElementReference', { read: ElementRef, static: false }) dismissButton!: ElementRef;
+
+  @ViewChild('itemFormComponentReference', { read: ItemFormComponent, static: false }) itemForm!: ItemFormComponent;
+  // @ViewChild('selectProductComponentReference', { read: SelectProductComponent, static: false }) selectProduct!: SelectProductComponent;
+
+  orderItemData: any;
+
+  isItemSaving = false;
+
   ngOnInit(): void {
   }
 
+  openModal(data: any){
+    this.orderItemData = data;
+
+    this.itemForm.itemForm.controls.itemNumber.setValue(data.item_number);
+    this.itemForm.itemForm.controls.productCode.setValue(data.product?.product_code);
+    this.itemForm.itemForm.controls.productName.setValue(data.product?.product_name);
+    this.itemForm.itemForm.controls.price.setValue(data.product.price);
+    this.itemForm.itemForm.controls.quantity.setValue(data.quantity);
+    this.itemForm.selectedProductId = data.product.id;
+    this.itemForm.selectedProductData = data.product;
+
+    this.editButton.nativeElement.click();
+  }
+
+  saveItem(){
+    // let data: OrderItem = {
+    let data = {
+      item_number: this.itemForm.itemForm.controls.itemNumber.value as number,
+      order: sessionStorage.getItem('shop_order_id') as string,
+      quantity: this.itemForm.itemForm.controls.quantity.value as number,
+      product: this.itemForm.selectedProductId,
+    }
+
+    let item = {
+      id: this.orderItemData.id,
+      data: data
+    }
+
+    this.saveItemEvent.emit(item);
+  }
+
+  openProductWindow(){
+    console.log("You are opening select menu item window")
+    // this.selectProduct.openModal();
+  }
+
+  onProductSelected(itemData: any){
+    console.log(itemData);
+    this.itemForm.selectedProductId = itemData.id;
+    this.itemForm.selectedProductData = itemData;
+
+    this.itemForm.itemForm.controls.productCode.setValue(itemData?.product?.product_code);
+    this.itemForm.itemForm.controls.productName.setValue(itemData?.product?.product_name);
+    this.itemForm.itemForm.controls.price.setValue(itemData.price);
+  }
+  
 }
